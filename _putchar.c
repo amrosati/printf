89204bd0d0da8fe0479b3c4c4fb300f3817577d7
Amro Satti <amrsatti98@gmail.com>
2023-07-06 22:21:50 +0300

    reimplemented the local buffer

--- _putchar.c
+++ _putchar.c
@@ -8,5 +8,19 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buff[BUFFSIZ];
+	static int i;
+
+	if  (c == -1 || i >= 1024)
+	{
+		write(1, &buff, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buff[i] = c;
+		i++;
+	}
+
+	return (1);
 }
